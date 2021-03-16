d68807027495a4e22892070002640d44e4678c5d
Javier Rodriguez <javier.rodriguez1@bp.com>
2021-03-16 20:25:42 +0000

    rename package

--- main.go
+++ main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
-	"github.com/javierropi/tf-aws-gateway-deployment/extendedapigateway"
+	"github.com/javierropi/tf-aws-gateway-deployment/extendedapigatewaydeployment"
 )
 
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: func() *schema.Provider {
-			return extendedapigateway.Provider()
+			return extendedapigatewaydeployment.Provider()
 		},
 	})
 }
