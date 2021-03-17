c941ca40eca4db073576784e38eb85b98d2c9072
pingzhou <pingzhou.liu@slalom.com>
2021-03-17 15:42:07 +0000

    update

--- apigwdeployment/provider.go
+++ apigwdeployment/provider.go
@@ -2,7 +2,7 @@ package apigwdeployment
 
 import (
 	"context"
-	
+
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
@@ -17,7 +17,7 @@ func Provider() *schema.Provider {
 				DefaultFunc: schema.EnvDefaultFunc("api_gw_aws_profile", nil),
 			},
 		},
-		ResourcesMap:         map[string]*schema.Resource{
+		ResourcesMap: map[string]*schema.Resource{
 			"apigwdeployment_methods": resourceAwsApiGatewayDeployment(),
 		},
 		DataSourcesMap:       map[string]*schema.Resource{},
@@ -30,8 +30,8 @@ func providerConfigure(ctx context.Context, d *schema.ResourceData) (interface{}
 
 	var diags diag.Diagnostics
 
-	cfg, err := config.LoadDefaultConfig(context.TODO(), 
-	config.WithSharedConfigProfile(profile))
+	cfg, err := config.LoadDefaultConfig(context.TODO(),
+		config.WithSharedConfigProfile(profile))
 	if err != nil {
 		diags = append(diags, diag.Diagnostic{
 			Severity: diag.Error,
--- apigwdeployment/resource_aws_api_gateway_deployment.go
+++ apigwdeployment/resource_aws_api_gateway_deployment.go
@@ -2,7 +2,7 @@ package apigwdeployment
 
 import (
 	"fmt"
-	"log"
+	"context"
 
 	//"time"
 
@@ -93,11 +93,8 @@ func resourceAwsApiGatewayDeployment() *schema.Resource {
 }
 
 func resourceAwsApiGatewayDeploymentCreate(d *schema.ResourceData, meta interface{}) error {
-	cfg:= meta.(aws.Config)
-
-	client:= apigateway.NewFromConfig(cfg)
-	// Create the gateway
-	log.Printf("[DEBUG] Creating API Gateway Deployment")
+	cfg := meta.(aws.Config)
+	client := apigateway.NewFromConfig(cfg)
 
 	variables := make(map[string]string)
 	for k, v := range d.Get("variables").(map[string]interface{}) {
@@ -108,12 +105,9 @@ func resourceAwsApiGatewayDeploymentCreate(d *schema.ResourceData, meta interfac
 	for k, v := range d.Get("canary_settings_stageVariableOverrides").(map[string]interface{}) {
 		variables[k] = v.(string)
 	}
-     
-	/*
-	// https://pkg.go.dev/github.com/aws/aws-sdk-go-v2/service/apigateway#Client.CreateDeployment
 	
 	var err error
-	deployment, err := client.CreateDeployment(&apigateway.CreateDeploymentInput{
+	deployment, err := client.CreateDeployment(context.TODO(), &apigateway.CreateDeploymentInput{
 		CacheClusterEnabled: new(bool),
 		CacheClusterSize:    new(string),
 		CanarySettings:      &apigateway.DeploymentCanarySettings{PercentTraffic: aws.Float64(d.Get("canary_settings_percentTraffic").(float64)), StageVariableOverrides: aws.StringMap(stageVariablesOverrrides), UseStageCache: aws.Bool(d.Get("canary_settings_percentTraffic").(bool))},
@@ -126,11 +120,7 @@ func resourceAwsApiGatewayDeploymentCreate(d *schema.ResourceData, meta interfac
 	})
 	if err != nil {
 		return fmt.Errorf("Error creating API Gateway Deployment: %s", err)
-	}*/
-	
-
-	d.SetId(aws.StringValue(deployment.Id))
-	log.Printf("[DEBUG] API Gateway Deployment ID: %s", d.Id())
+	}
 
 	return nil
 	//return resourceAwsApiGatewayDeploymentRead(d, meta)
